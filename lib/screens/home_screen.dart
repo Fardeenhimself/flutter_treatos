936d91fedcc096f01b695d80dd2cc056f3317d95
fardeenhimself <fardeenviper77@gmail.com>
2025-08-04 01:22:48 +0600

    added a carousel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,16 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:treatos_bd/widgets/main_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  // List of images for carousel
+  final List<String> bannerImage = [
+    'assets/banner-1.jpg',
+    'assets/banner-2.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +24,78 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       drawer: MainDrawer(),
-      body: Center(child: Text('Welcome to treatos_bd')),
+      body: Column(
+        children: [
+          // Banner Slider
+          CarouselSlider(
+            items: bannerImage.map((imagePath) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadiusGeometry.circular(12),
+                        child: Image.asset(
+                          imagePath,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+
+                      // Button on the banner
+                      Positioned(
+                        bottom: 10,
+                        right: 10,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.purple,
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 12,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          onPressed: () {
+                            // To take to categories
+                          },
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Text(
+                                'Shop now',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(width: 6),
+                              FaIcon(
+                                FontAwesomeIcons.arrowRight,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            }).toList(),
+            options: CarouselOptions(
+              height: 180,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 5),
+              enlargeCenterPage: true,
+              viewportFraction: 0.9,
+            ),
+          ),
+          // Content
+          Center(child: Text('Welcome to treatos_bd')),
+        ],
+      ),
     );
   }
 }
